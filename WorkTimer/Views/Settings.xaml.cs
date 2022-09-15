4e9858f452ba44c895150f37f2c2a0e52762d29d
Jack Holm <jackdholm1@gmail.com>
2022-09-15 10:20:56 -0500

    Added highlighting when selecting time inputs on settings

--- WorkTimer/Views/Settings.xaml.cs
+++ WorkTimer/Views/Settings.xaml.cs
@@ -19,9 +19,19 @@ namespace WorkTimer.Views
     /// </summary>
     public partial class Settings : Window
     {
+        private SolidColorBrush _defaultBackground;
+        private SolidColorBrush _focusedBackground;
+        private SolidColorBrush _defaultForeground;
+        private SolidColorBrush _focusedForeground;
+
         public Settings()
         {
             InitializeComponent();
+            _defaultBackground = new SolidColorBrush(Colors.White);
+            _focusedBackground = new SolidColorBrush(Colors.DarkGoldenrod);
+            _defaultForeground = new SolidColorBrush(Colors.Black);
+            _focusedForeground = new SolidColorBrush(Colors.White);
+            HoursInput.CaretBrush = MinutesInput.CaretBrush = _focusedBackground;
         }
 
         private void HoursInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -42,6 +52,14 @@ namespace WorkTimer.Views
         private void HoursInput_GotFocus(object sender, RoutedEventArgs e)
         {
             HoursInput.CaretIndex = 2;
+            HoursInput.Background = _focusedBackground;
+            HoursInput.Foreground = _focusedForeground;
+        }
+
+        private void HoursInput_LostFocus(object sender, RoutedEventArgs e)
+        {
+            HoursInput.Background = _defaultBackground;
+            HoursInput.Foreground = _defaultForeground;
         }
 
         private void HoursInput_SelectionChanged(object sender, RoutedEventArgs e)
@@ -52,6 +70,14 @@ namespace WorkTimer.Views
         private void MinutesInput_GotFocus(object sender, RoutedEventArgs e)
         {
             MinutesInput.CaretIndex = 2;
+            MinutesInput.Background = _focusedBackground;
+            MinutesInput.Foreground = _focusedForeground;
+        }
+
+        private void MinutesInput_LostFocus(object sender, RoutedEventArgs e)
+        {
+            MinutesInput.Background = _defaultBackground;
+            MinutesInput.Foreground = _defaultForeground;
         }
 
         private void MinutesInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -73,5 +99,6 @@ namespace WorkTimer.Views
                 MinutesInput.Text = "0" + e.Text;
             MinutesInput.CaretIndex = 2;
         }
+
     }
 }
